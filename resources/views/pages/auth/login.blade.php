15d9720f9d37f2bb30d6df968306d40e80aba9f8
IchalAlwiros <ichalwiradev@gmail.com>
2024-06-01 21:44:40 +0700

    add validate message form login

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -12,7 +12,6 @@
     <div class="card card-primary">
         <div class="card-header">
             <h4>Login</h4>
-            {{-- <p>Masuk untuk melanjutkan</p> --}}
         </div>
 
         <div class="card-body">
@@ -23,44 +22,29 @@
                 @csrf
                 <div class="form-group">
                     <label for="email">Email</label>
-                    <input id="email"
-                        type="email"
-                        class="form-control"
-                        name="email"
-                        tabindex="1"
-                        required
-                        autofocus>
-                    <div class="invalid-feedback">
-                        Please fill in your email
-                    </div>
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                        value="{{ old('email') }}" name="email" tabindex="1">
+                    @error('email')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+
                 </div>
 
                 <div class="form-group">
-                    {{-- <div class="d-block">
-                        <label for="password"
-                            class="control-label">Password</label>
-                        <div class="float-right">
-                            <a href="auth-forgot-password.html"
-                                class="text-small">
-                                Forgot Password?
-                            </a>
-                        </div>
-                    </div> --}}
-                    <input id="password"
-                        type="password"
-                        class="form-control"
-                        name="password"
-                        tabindex="2"
-                        required>
+                    <div class="d-block">
+                        <label for="password" class="control-label">Password</label>
+
+                    </div>
+                    <input id="password" type="password" class="form-control" name="password" tabindex="2">
                     <div class="invalid-feedback">
                         please fill in your password
                     </div>
                 </div>
 
                 <div class="form-group">
-                    <button type="submit"
-                        class="btn btn-primary btn-lg btn-block"
-                        tabindex="4">
+                    <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
                         Login
                     </button>
                 </div>
